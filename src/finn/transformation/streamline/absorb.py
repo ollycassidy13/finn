67d7ac3d8dc1a69026d0dfde8a7a4c29adce79ba
ollycassidy13 <ollyj.cassidy@gmail.com>
2026-07-28 09:52:11 +0100

    Preserve ternary datatype for asymmetric quantization

--- src/finn/transformation/streamline/absorb.py
+++ src/finn/transformation/streamline/absorb.py
@@ -81,7 +81,10 @@ class AbsorbSignBiasIntoMultiThreshold(Transformation):
                         continue
                     for candidate in DataType.get_accumulator_dt_cands():
                         odt = DataType[candidate]
-                        if odt.allowed(new_min) and odt.allowed(new_max):
+                        if all(
+                            odt.allowed(value)
+                            for value in range(int(new_min), int(new_max) + 1)
+                        ):
                             break
                     else:
                         raise AssertionError(
